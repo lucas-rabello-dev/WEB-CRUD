a8d2d6143c1c1992667a9c7cb5e0eb6170f2b4c5
lucas-rabello-dev <lucasrabello2020@gmail.com>
2025-12-16 20:13:48 -0300

    Update model | adicionado draft.txt com features nele

--- model/model.go
+++ model/model.go
@@ -1,11 +1,26 @@
 package model
 
-
 type Livro struct {
-	Nome string
-	NumPages int
+	Titulo string
+	Autor string
 	Idioma string
+	NumPages int
 	Editora string
-	DataPublicacao string // data de publicação do livro
-	LinkAmazon string
+	Genero string
+	AnoPublicacao string // data de publicação do livro
+	LinkCompra []string // pode ser um ou mais
+	Sinopse string
 }
+
+type User struct {
+	NomeCompleto string
+	Idade int
+	CPF string
+	Email string
+	Email_Rec string // email de recuperação se precisar
+	senha string
+	IsAdmin bool
+}
+
+
+// escrever o getter e setter para o atributo senha
