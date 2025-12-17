75baae0fc50ca515dd31bc55d70b797d7f52f699
lucas-rabello-dev <lucasrabello2020@gmail.com>
2025-12-17 13:26:15 -0300

    Revisão de ideias

--- model/model.go
+++ model/model.go
@@ -10,6 +10,11 @@ type Livro struct {
 	AnoPublicacao string // data de publicação do livro
 	LinkCompra []string // pode ser um ou mais
 	Sinopse string
+	ValorMultaDiaria float64
+}
+
+func (l *Livro) CalcularMulta(valorOriginal float64) {
+	l.ValorMultaDiaria = valorOriginal * 30 / 100
 }
 
 type User struct {
