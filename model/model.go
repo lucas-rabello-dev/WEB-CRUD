a14fd16cde2c5bf9c9b89ec58241d2547e165a10
lucas-rabello-dev <lucasrabello2020@gmail.com>
2025-12-18 19:51:54 -0300

    feito login com IA

--- model/model.go
+++ model/model.go
@@ -1,15 +1,15 @@
 package model
 
 type Livro struct {
-	Titulo string
-	Autor string
-	Idioma string
-	NumPages int
-	Editora string
-	Genero string
-	AnoPublicacao string // data de publicação do livro
-	LinkCompra []string // pode ser um ou mais
-	Sinopse string
+	Titulo           string
+	Autor            string
+	Idioma           string
+	NumPages         int
+	Editora          string
+	Genero           string
+	AnoPublicacao    string   // data de publicação do livro
+	LinkCompra       []string // pode ser um ou mais
+	Sinopse          string
 	ValorMultaDiaria float64
 }
 
@@ -19,13 +19,11 @@ func (l *Livro) CalcularMulta(valorOriginal float64) {
 
 type User struct {
 	NomeCompleto string
-	Idade int
-	CPF string
-	Email string
-	Email_Rec string // email de recuperação se precisar
-	senha string
-	IsAdmin bool
+	Idade        int
+	CPF          string
+	Email        string
+	Email_Rec    string // email de recuperação se precisar
+	senha        string
 }
-
 
 // escrever o getter e setter para o atributo senha
